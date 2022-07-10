81cd2f1aacab572bbafa856327e569733f4b8aa0
Maxim <kejerino@yandex.ru>
2022-07-10 18:24:09 +0800

    fix: fixed errors

--- app.js
+++ app.js
@@ -33,17 +33,17 @@ app.use(limiter);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().email().required(),
+    email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 }), login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().custom(regExp, 'custom validation'),
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
   }),
 }), createUser);
 
